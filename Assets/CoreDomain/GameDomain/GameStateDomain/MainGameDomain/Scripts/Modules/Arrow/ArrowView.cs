8b54eb24b0a02e50d5265452059f1b75428dc8a6
razhollander <50023823+razhollander@users.noreply.github.com>
2023-12-13 11:22:19 +0200

    wip arrow stab

--- Assets/CoreDomain/GameDomain/GameStateDomain/MainGameDomain/Scripts/Modules/Arrow/ArrowView.cs
+++ Assets/CoreDomain/GameDomain/GameStateDomain/MainGameDomain/Scripts/Modules/Arrow/ArrowView.cs
@@ -1,33 +1,47 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using CoreDomain.Scripts.Extensions;
 using UnityEngine;
-using DG.Tweening;
+
 public class ArrowView : MonoBehaviour
 {
     [SerializeField] private Rigidbody _rigidbody;
     [SerializeField] private float _jumpForce=5;
     [SerializeField] private float _jumpAngleRelativeToFloor = 60;
     [SerializeField] private Transform _rendererTransform;
-    [SerializeField] private float _endZRotationAngleRelativeToFloor = -75;
-    [SerializeField] private float _currentZRotation;
-    [SerializeField] private float _destinationZRotation;
     [SerializeField] private float _startRotationLoopSpeed = 1;
     [SerializeField] private float _spacePressedRotationLoopSpeed = 1;
     [SerializeField] private float _angularDrag = 5;
-    [SerializeField] private float _angularDragPower = 1f;
-
-    private bool _didPassCriticalAngleInCurrentLoop = false;
+    [SerializeField] private Collider _arrowHeadCollider;
+    [SerializeField] private float _maxStabAngle = 45;
     
     private float _prevZRotation;
-    // Start is called before the first frame update
-    void Start()
+
+    private void Awake()
     {
-        
+        _rigidbody.angularDrag = _angularDrag;
+        //_arrowHeadCollider.on
     }
 
-    // Update is called once per frame
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Enter!");
+        
+        if (collision.contacts.Length > 0 && DidStabContactPoint(collision.contacts[0]))
+        {
+            Debug.Log("Stab!");
+            _rigidbody.velocity = Vector3.zero;
+            _rigidbody.angularVelocity = Vector3.zero;
+            _rigidbody.isKinematic = true;
+        }
+    }
+
+    private bool DidStabContactPoint(ContactPoint contactPoint)
+    {
+        Vector3 normal = contactPoint.normal;
+        Vector3 worldForward = transform.TransformDirection(_rendererTransform.transform.forward);
+
+        return Vector3.Angle(worldForward, normal) < _maxStabAngle;
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
@@ -38,49 +52,53 @@ public class ArrowView : MonoBehaviour
 
     private void FixedUpdate()
     {
-        _rigidbody.angularDrag = _angularDrag;
+        var currentZRotation = ConvertAngleToBeBetween0To360(transform.eulerAngles.z);
+        var shouldStartANewLoop = _prevZRotation is < 280 and > 270 && currentZRotation is > 260 and <= 270;
+        
+        if (shouldStartANewLoop)
+        {
+            SetLoopAngularVelocity();
+        }
 
-        var _currentZRotation = transform.eulerAngles.z;
-        var between0To360 = _currentZRotation % 360f;
+        _prevZRotation = currentZRotation;
+    }
+
+    private float ConvertAngleToBeBetween0To360(float angle)
+    {
+        var between0To360 = angle % 360f;
          
         if (between0To360 < 0)
         {
             between0To360 += 360;
         }
 
-        bool shouldStartANewLoop = 280 >_prevZRotation && _prevZRotation > 270 && 260 < between0To360 && between0To360 < 270;
-        
-        if (shouldStartANewLoop)
-        {
-            Debug.Log($"Start! _prevZRotation: {_prevZRotation}, between0To360: {between0To360}");
-            _rigidbody.angularVelocity = new Vector3(0, 0, _startRotationLoopSpeed);
-        }
-        
-        //if (!_rigidbody.angularVelocity.z.EqualsWithTolerance(0) && between0To360 < 180 && between0To360 > 0)
-        //{
-        //    Debug.Log($"angle: {between0To360}");
-        //    //_rigidbody.angularDrag = Mathf.Lerp(180 - between0To360, 180, _angularDrag) /180 * _angularDragPower;
-        //    _rigidbody.angularDrag = Mathf.Pow((180 - between0To360)/180, _angularDrag)  * _angularDragPower;
-        //}
-
-        _prevZRotation = between0To360;
+        return between0To360;
     }
 
-    private void DoRotateLoop()
-    {
-        _rigidbody.angularVelocity = new Vector3(0, 0, _spacePressedRotationLoopSpeed);
-    }
-    
     private void Jump()
     {
-        _rigidbody.velocity = CalcJumpVector();
-        DoRotateLoop();
-
-        //_rigidbody.AddForce(CalcJumpVector());
+        _rigidbody.isKinematic = false;
+        SetJumpVelocity();
+        SetJumpAngularVelocity();
     }
 
-    private Vector3 CalcJumpVector()
+    private void SetJumpAngularVelocity()
     {
-        return Quaternion.Euler(0, 0, _jumpAngleRelativeToFloor) * Vector3.right * _jumpForce;
+        SetZAngularVelocity(_spacePressedRotationLoopSpeed);
+    }
+    
+    private void SetLoopAngularVelocity()
+    {
+        SetZAngularVelocity(_startRotationLoopSpeed);
+    }
+
+    private void SetZAngularVelocity(float angularVelocity)
+    {
+        _rigidbody.angularVelocity = new Vector3(0, 0, angularVelocity);
+    }
+    
+    private void SetJumpVelocity()
+    {
+        _rigidbody.velocity = Quaternion.Euler(0, 0, _jumpAngleRelativeToFloor) * Vector3.right * _jumpForce;
     }
 }
