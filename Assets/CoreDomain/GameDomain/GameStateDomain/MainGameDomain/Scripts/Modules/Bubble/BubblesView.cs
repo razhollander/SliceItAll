afe5f01537be2069b6dec03be1fabce6000b6efe
razhollander <50023823+razhollander@users.noreply.github.com>
2023-12-16 22:06:57 +0200

    level2

--- Assets/CoreDomain/GameDomain/GameStateDomain/MainGameDomain/Scripts/Modules/Bubble/BubblesView.cs
+++ Assets/CoreDomain/GameDomain/GameStateDomain/MainGameDomain/Scripts/Modules/Bubble/BubblesView.cs
@@ -5,51 +5,5 @@ using UnityEngine;
 
 public class BubblesView : MonoBehaviour
 {
-    [SerializeField] private ParticleSystem _bubblesParticleSystem;
-
-    // these lists are used to contain the particles which match
-    // the trigger conditions each frame.
-    List<ParticleSystem.Particle> enter = new List<ParticleSystem.Particle>();
-
-    void OnParticleCollision(GameObject other)
-    {
-        //Debug.Log("OnParticleCollision");
-//
-        //// get the particles which matched the trigger conditions this frame
-        //int numEnter = _bubblesParticleSystem.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
-//
-        //// iterate through the particles which entered the trigger and make them red
-        //for (int i = 0; i < numEnter; i++)
-        //{
-        //    ParticleSystem.Particle p = enter[i];
-        //    Debug.Log(p.position);
-        //    //p.startColor = new Color32(255, 0, 0, 255);
-        //    //p.position
-        //    enter[i] = p;
-        //}
-//
-        //// re-assign the modified particles back into the particle system
-        //_bubblesParticleSystem.SetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
-    }
-
-    void OnParticleTrigger()
-    {
-        Debug.Log("OnParticleTrigger");
-
-        // get the particles which matched the trigger conditions this frame
-        int numEnter = _bubblesParticleSystem.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
-
-        // iterate through the particles which entered the trigger and make them red
-        for (int i = 0; i < numEnter; i++)
-        {
-            ParticleSystem.Particle p = enter[i];
-            Debug.Log(p.position);
-            //p.startColor = new Color32(255, 0, 0, 255);
-            //p.position
-            enter[i] = p;
-        }
-
-        // re-assign the modified particles back into the particle system
-        _bubblesParticleSystem.SetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
-    }
+    
 }
