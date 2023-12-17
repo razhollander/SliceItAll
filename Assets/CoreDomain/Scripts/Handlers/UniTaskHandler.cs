bbed2df1996b2f319e08bcbfd98d0c06239d6f3c
razhollander <50023823+razhollander@users.noreply.github.com>
2023-12-17 21:24:24 +0200

    adjustments

--- Assets/CoreDomain/Scripts/Handlers/UniTaskHandler.cs
+++ Assets/CoreDomain/Scripts/Handlers/UniTaskHandler.cs
@@ -6,10 +6,6 @@ public static class UniTaskHandler
 {
      public static async UniTask WaitForAnyKeyPressed()
      {
-#if UNITY_EDITOR
          await Observable.EveryUpdate().Where(_ => Input.anyKeyDown).First().ToTask();
-#else
-         await Observable.EveryUpdate().Where(_ => Input.GetMouseButtonDown(0)).First().ToTask();
-#endif
      }
 }
